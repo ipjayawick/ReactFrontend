6a32ec8dfea4ce7f1dfbd5ccc6e69e3194b6db7d
ipjayawick <ipjayawick@gmail.com>
2025-04-18 15:23:58 +0530

    code refractored

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api/tasks';   
+const API_BASE_URL = 'http://localhost:8080/api/tasks';
 
 // Fetch all tasks
 export const fetchTasks = async () => {
--- src/api/taskapi.js
+++
@@ -1,6 +0,0 @@
-import axios from './axios';
-
-export const getAllTasks = () => axios.get('/tasks');
-export const createTask = (task) => axios.post('/tasks', task);
-export const updateTask = (id, task) => axios.put(`/tasks/${id}`, task);
-export const deleteTask = (id) => axios.delete(`/tasks/${id}`);
